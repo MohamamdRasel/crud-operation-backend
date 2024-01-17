1834f0868f59c9bb3f579630c737eec5bd0a5f47
Mohammad <mohammadrasel201@gmail.com>
2024-01-17 22:18:55 +0600

    completed 55

--- src/Components/Update.jsx
+++ src/Components/Update.jsx
@@ -10,6 +10,22 @@ const Update = () => {
         const name = form.name.value;
         const email = form.email.value;
         console.log(name, email);
+        const UpdatedUser = { name, email };
+
+        fetch(`http://localhost:5000/users/${loadedUser._id}`, {
+            method: 'PUT',
+            headers: {
+                'content-type' : 'application/json'
+            },
+            body: JSON.stringify(UpdatedUser)
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                if (data.modifiedCount>0){
+                    alert('user updated successfully')
+                }
+            })
 
     }
 
